bbfe255344b5742716dbb0831f7fb81a9ba05200
Dmitriy <shalberkind@mail.ru>
2020-07-22 14:12:37 +0300

    Yoga - Добавлен скрипт для модального окна

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -75,6 +75,23 @@ window.addEventListener('DOMContentLoaded', function () {
   }
   setClock('timer', deadline);
 
+  // popup
+
+  let moreBtn = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    closeBtn = document.querySelector('.popup-close');
+
+  moreBtn.addEventListener('click', function () {
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+
+  closeBtn.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    moreBtn.classList.remove('more-splash');
+    document.body.style.overflow = '';
+  });
 
 
 });
