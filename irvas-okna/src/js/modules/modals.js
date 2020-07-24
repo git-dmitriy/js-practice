130d4161ff3ac7468378a21c5ffbc45d9d1dca51
Dmitriy <shalberkind@mail.ru>
2020-07-24 23:19:37 +0300

    Ирвас окна - Создание скрипта для модальных окон

--- irvas-okna/src/js/modules/modals.js
+++ irvas-okna/src/js/modules/modals.js
@@ -1,31 +1,37 @@
 const modals = () => {
-  function bindModal(trigger, modal, close) {
-    trigger.addEventListener('click', (e) => {
-      if (e.target) {
-        e.preventDefault();
-      }
-      modal.style.display = 'block';
-      document.body.overflow = 'hidden';
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        modal.style.display = 'block';
+        document.body.overflow = 'hidden';
+      });
     });
+
     close.addEventListener('click', () => {
       modal.style.display = 'none';
       document.body.overflow = '';
+      // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
         document.body.overflow = '';
+        // document.body.classList.remove('modal-open');
       }
     });
 
   }
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-    modalEngineer = document.querySelector('.popup_engineer'),
-    modalEngineerclose = document.querySelector('.popup_engineer .popup_close');
-
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerclose);
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
 };
 
 export default modals;
