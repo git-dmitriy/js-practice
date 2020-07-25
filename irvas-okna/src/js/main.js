340c21fd2875da48012d68a855ed60d7f408205e
Dmitriy <shalberkind@mail.ru>
2020-07-25 11:08:35 +0300

    Ирвас окна - Подключен скрипт для управления табами

--- irvas-okna/src/js/main.js
+++ irvas-okna/src/js/main.js
@@ -1,7 +1,9 @@
 import './slider';
 import modals from './modules/modals';
+import tabs from './modules/tabs';
+
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
+  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 });
-
