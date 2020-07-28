2526e1692d9f5f1064f3fb3086edcc99ed5473ce
Dmitriy <shalberkind@mail.ru>
2020-07-28 21:34:35 +0300

    Ирвас окна - Модернизирован скрипт модальных окон -> добавлено условие закрытия окна по клику на overlay

--- irvas-okna/src/js/modules/modals.js
+++ irvas-okna/src/js/modules/modals.js
@@ -1,27 +1,38 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]');
 
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
         if (e.target) {
           e.preventDefault();
         }
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
       });
     });
 
     close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
       modal.style.display = 'none';
       document.body.overflow = '';
       // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
         modal.style.display = 'none';
         document.body.overflow = '';
         // document.body.classList.remove('modal-open');
@@ -37,8 +48,11 @@ const modals = () => {
   }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+
   bindModal('.phone_link', '.popup', '.popup .popup_close');
+
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+
   showModalOnTimer('.popup', 60000);
 };
 
