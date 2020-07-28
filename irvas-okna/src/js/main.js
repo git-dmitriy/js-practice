bd44342bffac14800a47d5649291c7acd1b4b940
Dmitriy <shalberkind@mail.ru>
2020-07-28 22:28:04 +0300

    Ирвас окна - Тестирование механизма скрипта

--- irvas-okna/src/js/main.js
+++ irvas-okna/src/js/main.js
@@ -2,12 +2,13 @@ import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
-
+import changeModalState from './modules/changeModalState';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   let modalState = {};
+  changeModalState(modalState);
 
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
--- irvas-okna/src/js/modules/changeModalState.js
+++ irvas-okna/src/js/modules/changeModalState.js
@@ -1,5 +1,17 @@
 const changeModalState = (state) => {
 
+  const windowShape = document.querySelectorAll('.balcon_icons_img'),
+    windowWidth = document.querySelector('#width'),
+    windowHeight = document.querySelector('#height'),
+    windowType = document.querySelector('#view_type'),
+    windowProfile = document.querySelectorAll('.checkbox');
+
+  windowShape.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      state.form = i;
+      console.log(state);
+    });
+  });
 };
 
 export default changeModalState;
