6ffc0e37294a45c425b660bdc5a8ee5c53d5708a
Dmitriy <shalberkind@mail.ru>
2020-07-30 22:53:10 +0300

    Ирвас окна - Добавлена функция сбора данных с полей для калькулятора

--- irvas-okna/src/js/modules/changeModalState.js
+++ irvas-okna/src/js/modules/changeModalState.js
@@ -3,9 +3,9 @@ import checkNumInputs from './checkNumInputs';
 const changeModalState = (state) => {
 
   const windowShape = document.querySelectorAll('.balcon_icons_img'),
-    windowWidth = document.querySelector('#width'),
-    windowHeight = document.querySelector('#height'),
-    windowType = document.querySelector('#view_type'),
+    windowWidth = document.querySelectorAll('#width'),
+    windowHeight = document.querySelectorAll('#height'),
+    windowType = document.querySelectorAll('#view_type'),
     windowProfile = document.querySelectorAll('.checkbox');
 
   checkNumInputs('#width');
@@ -14,13 +14,38 @@ const changeModalState = (state) => {
   function bindActionToElements(event, element, prop) {
     element.forEach((item, i) => {
       item.addEventListener(event, () => {
-        state[prop] = i;
+        switch (item.nodeName) {
+          case 'SPAN':
+            state[prop] = i;
+            break;
+          case 'INPUT':
+            if (item.getAttribute('type') === 'checkbox') {
+              i === 0 ? state[prop] = 'Холодное' : state[prop] = 'Теплое';
+              element.forEach((box, j) => {
+                box.checked = false;
+                if (i == j) {
+                  box.checked = true;
+                }
+              });
+
+            } else {
+              state[prop] = item.value;
+            }
+            break;
+          case 'SELECT':
+            state[prop] = item.value;
+            break;
+        }
         console.log(state);
+
       });
     });
   }
-
-  bindActionToElements('click', windowShape, 'form');
+  bindActionToElements('click', windowShape, 'shape');
+  bindActionToElements('input', windowHeight, 'height');
+  bindActionToElements('input', windowWidth, 'width');
+  bindActionToElements('change', windowType, 'type');
+  bindActionToElements('change', windowProfile, 'profile');
 
 };
 
