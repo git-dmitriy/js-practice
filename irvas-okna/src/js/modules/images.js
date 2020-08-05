87997d57b8a0ccc28f77af5357714673463d7ff5
Dmitriy <shalberkind@mail.ru>
2020-08-05 13:54:08 +0300

    Ирвас окна - Добавлено ограничение по ширине открываемого изображения и запрет прокрутки страницы при открытии

--- irvas-okna/src/js/modules/images.js
+++ irvas-okna/src/js/modules/images.js
@@ -1,42 +1,44 @@
 const images = () => {
-
   /*
-  TODO Отключить прокрутку страницы при открытии изображения
+  TODO + Отключить прокрутку страницы при открытии изображения
 
-  TODO Контролировать размер изображения в модальном окне
+  TODO (+-) Контролировать размер изображения в модальном окне
+         Добавлено не больше 50vw;
 
   */
 
+  const imgPopup = document.createElement("div"),
+    workSection = document.querySelector(".works"),
+    bigImage = document.createElement("img");
 
-  const imgPopup = document.createElement('div'),
-    workSection = document.querySelector('.works'),
-    bigImage = document.createElement('img');
-
-  imgPopup.classList.add('popup');
-  workSection.appendChild(imgPopup);
-
-  imgPopup.style.justifyContent = 'center';
-  imgPopup.style.alignItems = 'center';
-  imgPopup.style.display = 'none';
-
-  imgPopup.appendChild(bigImage);
-
-  workSection.addEventListener('click', (e) => {
+  workSection.addEventListener("click", (e) => {
     e.preventDefault();
+
+    imgPopup.classList.add("popup");
+    workSection.appendChild(imgPopup);
+
+    imgPopup.style.justifyContent = "center";
+    imgPopup.style.alignItems = "center";
+    imgPopup.style.display = "none";
+
+    imgPopup.appendChild(bigImage);
 
     let target = e.target;
 
-    if (target && target.classList.contains('preview')) {
-      imgPopup.style.display = 'flex';
-      const path = target.parentNode.getAttribute('href');
-      bigImage.setAttribute('src', path);
+    if (target && target.classList.contains("preview")) {
+      imgPopup.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+      console.log(document.body);
+      const path = target.parentNode.getAttribute("href");
+      bigImage.setAttribute("src", path);
+      bigImage.style.maxWidth = "50vw";
     }
 
-    if (target && target.matches('div.popup')) {
-      imgPopup.style.display = 'none';
+    if (target && target.matches("div.popup")) {
+      imgPopup.style.display = "none";
+      document.body.style.overflowY = "";
     }
   });
-
 };
 
 export default images;
