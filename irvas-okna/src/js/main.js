236ea70c076b5fbec75dfffe99cb0c7fd5016157
Dmitriy <shalberkind@mail.ru>
2020-08-05 22:08:01 +0300

    Ирвас окна - Добавлена TODO задача на будущее

--- irvas-okna/src/js/main.js
+++ irvas-okna/src/js/main.js
@@ -1,30 +1,37 @@
-import './slider';
-import modals from './modules/modals';
-import tabs from './modules/tabs';
-import forms from './modules/forms';
-import changeModalState from './modules/changeModalState';
-import timer from './modules/timer';
-import images from './modules/images';
+import "./slider";
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
+import forms from "./modules/forms";
+import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
+import images from "./modules/images";
 
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
   let modalState = {};
-  let deadline = '2021-01-01T00:00:00';
+  let deadline = "2021-01-01T00:00:00";
 
   changeModalState(modalState);
   modals();
-  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-  tabs('.decoration_slider', '.no_click',
-    '.decoration_content > div > div', 'after_click');
+  tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+  tabs(
+    ".decoration_slider",
+    ".no_click",
+    ".decoration_content > div > div",
+    "after_click"
+  );
 
-  tabs('.balcon_icons', '.balcon_icons_img',
-    '.big_img > img', 'do_image_more', 'inline-block');
+  tabs(
+    ".balcon_icons",
+    ".balcon_icons_img",
+    ".big_img > img",
+    "do_image_more",
+    "inline-block"
+  );
 
   forms(modalState);
-  timer('.container1', deadline);
+  timer(".container1", deadline);
   images();
 });
 
@@ -35,5 +42,7 @@ TODO + Очистка объекта после отправки формы
 
 TODO Валидация формы на заполнение всех полей
 
+TODO Добавить проверку открытых модальных окон,
+     для функции showModalOnTimer
 
 */
