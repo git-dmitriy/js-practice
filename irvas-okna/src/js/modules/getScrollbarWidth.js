b1fa8ec1468c930e39df4a0118c1437e009bad14
Dmitriy <shalberkind@mail.ru>
2020-08-05 22:05:06 +0300

    Ирвас окна - Создание модуля рассчитывающего ширину полосы прокрутки страницы

---
+++ irvas-okna/src/js/modules/getScrollbarWidth.js
@@ -0,0 +1,17 @@
+function getScrollbarWidth() {
+  let div = document.createElement("div");
+
+  div.style.width = "50px";
+  div.style.height = "50px";
+  div.style.overflowY = "scroll";
+  div.style.visibility = "hidden";
+
+  document.body.appendChild(div);
+
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
+
+export default getScrollbarWidth;
