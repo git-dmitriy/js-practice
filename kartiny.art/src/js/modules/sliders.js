874ace731be73abc673d645cf823438871ba7447
Dmitriy <shalberkind@mail.ru>
2020-08-12 13:54:35 +0300

    Kartiny.art - Добавлена функция отключения автопролистывания слайдеров

--- kartiny.art/src/js/modules/sliders.js
+++ kartiny.art/src/js/modules/sliders.js
@@ -1,5 +1,6 @@
 const sliders = (slides, dir, prev, next) => {
-  let slideIndex = 1;
+  let slideIndex = 1,
+    paused = false;
 
   const items = document.querySelectorAll(slides);
 
@@ -42,18 +43,29 @@ const sliders = (slides, dir, prev, next) => {
     });
   } catch (e) {}
 
-  if (dir === "vertical") {
-    setInterval(function () {
-      selectSlide(1);
-      items[slideIndex - 1].classList.add("slideInDown");
-    }, 3000);
-  } else {
-    setInterval(function () {
-      selectSlide(1);
-      items[slideIndex - 1].classList.remove("slideInRight");
-      items[slideIndex - 1].classList.add("slideInLeft");
-    }, 3000);
+  function activateAnimation() {
+    if (dir === "vertical") {
+      paused = setInterval(function () {
+        selectSlide(1);
+        items[slideIndex - 1].classList.add("slideInDown");
+      }, 3000);
+    } else {
+      paused = setInterval(function () {
+        selectSlide(1);
+        items[slideIndex - 1].classList.remove("slideInRight");
+        items[slideIndex - 1].classList.add("slideInLeft");
+      }, 3000);
+    }
   }
+  activateAnimation();
+
+  items[0].parentNode.addEventListener("mouseenter", () => {
+    clearInterval(paused);
+  });
+
+  items[0].parentNode.addEventListener("mouseleave", () => {
+    activateAnimation();
+  });
 };
 
 export default sliders;
