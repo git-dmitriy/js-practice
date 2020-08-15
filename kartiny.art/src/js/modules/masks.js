0022ebf3f84736e68857d91f56b5a66270d23c5e
Dmitriy <shalberkind@mail.ru>
2020-08-15 13:42:19 +0300

    Kartiny.art - функция создания маски для обработки номера телефона

--- kartiny.art/src/js/modules/masks.js
+++ kartiny.art/src/js/modules/masks.js
@@ -1,5 +1,23 @@
 const masks = (selector) => {
+  function createMask(event) {
+    let matrix = "+7 (___) ___ __ __",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
 
+    if (def.length >= val.length) {
+      val = def;
+    }
+
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length
+        ? val.charAt(i++)
+        : i >= val.length
+        ? ""
+        : a;
+    });
+
+  }
 };
 
 export default masks;
