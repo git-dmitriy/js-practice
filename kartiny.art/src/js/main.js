ed398c97c0a53858c37060968a1b97b90381a925
Dmitriy <shalberkind@mail.ru>
2020-08-15 15:33:05 +0300

    Kartiny.art - Добавлен обработчик событий для input, и подключен модуль masks

--- kartiny.art/src/js/main.js
+++ kartiny.art/src/js/main.js
@@ -1,6 +1,7 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import masks from "./modules/masks";
 
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -9,6 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
   sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn");
   sliders(".main-slider-item", "vertical");
   forms();
+  masks("[name='phone']");
 });
 
 // TODO Убрать эффект смещения виджета подарка при открытии и закрытии модальных окон.
+
+// TODO Добавить минимальный индекс положения курсора в форме
+// TODO Пофиксить баг с focus в chrome
+// TODO Исключить появление модальных окон при заполнении форм
--- kartiny.art/src/js/modules/masks.js
+++ kartiny.art/src/js/modules/masks.js
@@ -1,4 +1,18 @@
 const masks = (selector) => {
+  let setCursorPosition = (pos, elem) => {
+    elem.focus();
+    if (elem.setSelectionRange) {
+      elem.setSelectionRange(pos, pos);
+    } else if (elem.createTextRange) {
+      let range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+    // elem.blur();
+  };
+
   function createMask(event) {
     let matrix = "+7 (___) ___ __ __",
       i = 0,
@@ -16,8 +30,23 @@ const masks = (selector) => {
         ? ""
         : a;
     });
-
+    if (event.type == "blur") {
+      if (this.value.length == 2) {
+        this.value = "";
+      } else {
+        setCursorPosition(this.value.length, this);
+        this.blur();
+      }
+    }
   }
+
+  let inputs = document.querySelectorAll(selector);
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", createMask);
+    input.addEventListener("focus", createMask);
+    input.addEventListener("blur", createMask);
+  });
 };
 
 export default masks;
