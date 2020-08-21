7455604070b8f48f9062475c0b075183582e66e9
Dmitriy <shalberkind@mail.ru>
2020-08-21 08:40:44 +0300

    Kartiny.art - Реализация аккордеона при помощи js и стилей

--- kartiny.art/src/js/main.js
+++ kartiny.art/src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
+import accordion from "./modules/accordion";
 
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   calc("#size", "#material", "#options", ".promocode", ".calc-price");
   filter();
   pictureSize(".sizes-block");
+  accordion(".accordion-heading", ".accordion-block");
 });
 
 // + TODO Убрать эффект смещения виджета подарка при открытии и закрытии модальных окон.
