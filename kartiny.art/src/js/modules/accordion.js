c0d46785eb0d23751ee9eaf7016ff877345bd082
Dmitriy <shalberkind@mail.ru>
2020-08-22 11:55:50 +0300

    Kartiny.art - Модернизация кода аккордеона

--- kartiny.art/src/js/modules/accordion.js
+++ kartiny.art/src/js/modules/accordion.js
@@ -3,6 +3,12 @@ const accordion = (triggersSelector) => {
 
   btns.forEach((btn) => {
     btn.addEventListener("click", function () {
+      btns.forEach((item) => {
+        item.classList.remove("active-style");
+        item.nextElementSibling.classList.remove("active-content");
+        item.nextElementSibling.style.maxHeight = "0";
+      });
+
       this.classList.toggle("active-style");
       this.nextElementSibling.classList.toggle("active-content");
 
