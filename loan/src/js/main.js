1e3817b740da59659ecb69427bfc267a043815c2
Dmitriy <shalberkind@mail.ru>
2020-08-29 23:37:39 +0300

    loan - Созданы экземпляры мини слайдеров

--- loan/src/js/main.js
+++ loan/src/js/main.js
@@ -1,10 +1,41 @@
 import MainSlider from "./modules/sliders/slider-main";
+import MiniSlider from "./modules/sliders/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const slider = new MainSlider({ page: ".page", btns: ".next" });
+  const slider = new MainSlider({ container: ".page", btns: ".next" });
   slider.render();
+
+  const showUpSlider = new MiniSlider({
+    container: ".showup__content-slider",
+    prev: ".showup__prev",
+    next: ".showup__next",
+    activeClass: "card-active",
+    animate: true,
+  });
+  showUpSlider.init();
+
+  const modulesSlider = new MiniSlider({
+    container: ".modules__content-slider",
+    prev: ".modules__info-btns .slick-prev",
+    next: ".modules__info-btns .slick-next",
+    activeClass: "card-active",
+    animate: true,
+  });
+  modulesSlider.init();
+
+  const feedSlider = new MiniSlider({
+    container: ".feed__slider",
+    prev: ".feed__slider .slick-prev",
+    next: ".feed__slider .slick-next",
+    activeClass: "feed__item-active",
+  });
+  feedSlider.init();
 
   const player = new VideoPlayer(".showup .play", ".overlay");
   player.init();
 });
+
+// * Добавить фильтрацию для третьего слайдера
+
+// todo Отключать автовоспроизведение слайдера
