58f431ce2ea66b8e89e9d9c75dd53df5c996ee89
Dmitriy <shalberkind@mail.ru>
2020-08-29 23:31:34 +0300

    loan - Добавлены новые свойства в объект слайдера

--- loan/src/js/modules/sliders/slider.js
+++ loan/src/js/modules/sliders/slider.js
@@ -4,12 +4,18 @@ export default class Slider {
     btns = null,
     prev = null,
     next = null,
+    activeClass = "",
+    animate,
+    autoPlay,
   } = {}) {
     this.container = document.querySelector(container);
     this.slides = this.container.children;
     this.btns = document.querySelectorAll(btns);
     this.prev = document.querySelector(prev);
     this.next = document.querySelector(next);
+    this.activeClass = activeClass;
+    this.animate = animate;
+    this.autoPlay = autoPlay;
     this.slideIndex = 1;
   }
 }
