ebac36470af43d2240bf437afb732fae3464a64e
Dmitriy <shalberkind@mail.ru>
2020-08-29 19:08:13 +0300

    loan - Добавлена функция анимации блоков внутри слайдера

--- loan/src/js/modules/sliders/slider-mini.js
+++ loan/src/js/modules/sliders/slider-mini.js
@@ -1,18 +1,37 @@
 import Slider from "./slider";
 
 export default class Minislider extends Slider {
-  constructor(container, next, prev) {
-    super(container, next, prev);
+  constructor(container, next, prev, activeClass, animate, autoPlay) {
+    super(container, next, prev, activeClass, animate, autoPlay);
+  }
+
+  decorized() {
+    this.slides.forEach((slide) => {
+      slide.classList.remove(this.activeClass);
+      if (this.animate) {
+        slide.querySelector(".card__controls").style.opacity = ".4";
+        slide.querySelector(".card__title").style.opacity = ".4";
+        slide.querySelector(".card__controls-arrow").style.opacity = ".4";
+      }
+    });
+    this.slides[0].classList.add(this.activeClass);
+    if (this.animate) {
+      this.slides[0].querySelector(".card__controls").style.opacity = "1";
+      this.slides[0].querySelector(".card__title").style.opacity = "1";
+      this.slides[0].querySelector(".card__controls-arrow").style.opacity = "1";
+    }
   }
 
   bindTriggers() {
     this.next.addEventListener("click", () => {
       this.container.appendChild(this.slides[0]);
+      this.decorized();
     });
 
     this.prev.addEventListener("click", () => {
       let active = this.slides[this.slides.length - 1];
       this.container.insertBefore(active, this.slides[0]);
+      this.decorized();
     });
   }
 
@@ -24,5 +43,6 @@ export default class Minislider extends Slider {
       align-items: flex-start;
     `;
     this.bindTriggers();
+    this.decorized();
   }
 }
