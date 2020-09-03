d5b072be058b8747cbcee4145bcb580b8ab07ff4
Dmitriy <shalberkind@mail.ru>
2020-09-03 18:07:04 +0300

    Добавлена разблокировка второго видео после просмотра превого

--- loan/src/js/modules/playVideo.js
+++ loan/src/js/modules/playVideo.js
@@ -19,46 +19,57 @@ export default class VidePlayer {
   }
 
   onPlayerStateChange(state) {
-    const blockedElement = this.activeBtn.closest(".module__video-item")
-      .nextElementSibling;
-    const playBntIcon = this.activeBtn.querySelector("svg").cloneNode(true);
+    try {
+      const blockedElement = this.activeBtn.closest(".module__video-item")
+        .nextElementSibling;
+      const playBntIcon = this.activeBtn.querySelector("svg").cloneNode(true);
 
-    if (state.data === 0) {
-      if (
-        blockedElement
-          .querySelector(".play__circle")
-          .classList.contains("closed")
-      ) {
-        blockedElement
-          .querySelector(".play__circle")
-          .classList.remove("closed");
-        blockedElement.querySelector("svg").remove();
-        blockedElement.querySelector(".play__circle").appendChild(playBntIcon);
-        blockedElement.querySelector(".play__text").textContent = "Play video";
-        blockedElement
-          .querySelector(".play__text")
-          .classList.remove("attention");
-        blockedElement.style.opacity = "1";
-        blockedElement.style.filter = "none";
+      if (state.data === 0) {
+        if (
+          blockedElement
+            .querySelector(".play__circle")
+            .classList.contains("closed")
+        ) {
+          blockedElement
+            .querySelector(".play__circle")
+            .classList.remove("closed");
+          blockedElement.querySelector("svg").remove();
+          blockedElement
+            .querySelector(".play__circle")
+            .appendChild(playBntIcon);
+          blockedElement.querySelector(".play__text").textContent =
+            "Play video";
+          blockedElement
+            .querySelector(".play__text")
+            .classList.remove("attention");
+          blockedElement.style.opacity = "1";
+          blockedElement.style.filter = "none";
+
+          blockedElement.setAttribute("data-disabled", "false");
+        }
       }
-    }
+    } catch (e) {}
   }
 
   bindTriggers() {
     this.btns.forEach((btn, i) => {
-      const blockedElement = btn.closest(".module__video-item")
-        .nextElementSibling;
+      try {
+        const blockedElement = btn.closest(".module__video-item")
+          .nextElementSibling;
 
-      this.activeBtn = btn;
-      if (i % 2 == 0) {
-        blockedElement.setAttribute("data-disabled", "true");
-      }
+        if (i % 2 == 0) {
+          blockedElement.setAttribute("data-disabled", "true");
+        }
+      } catch (e) {}
 
       btn.addEventListener("click", () => {
         if (
+          !btn.closest(".module__video-item") ||
           btn.closest(".module__video-item").getAttribute("data-disabled") !==
-          "true"
+            "true"
         ) {
+          this.activeBtn = btn;
+
           if (document.querySelector("iframe#frame")) {
             this.overlay.style.display = "flex";
             if (this.path !== btn.getAttribute("data-url")) {
