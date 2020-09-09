23ea1359ee1752049cd3f383ac04a569eb55f111
Dmitriy <shalberkind@mail.ru>
2020-09-09 13:04:25 +0300

    Базовая реализация цепочки вызовов

--- UI library/src/js/lib/core.js
+++ UI library/src/js/lib/core.js
@@ -2,7 +2,24 @@
   const $ = function (selector) {
     const elements = document.querySelectorAll(selector);
     console.log(elements);
-  };
 
+    const obj = {};
+
+    obj.hide = () => {
+      elements.forEach((element) => {
+        element.style.display = "none";
+      });
+      return obj;
+    };
+
+    obj.show = () => {
+      elements.forEach((element) => {
+        element.style.display = "";
+      });
+      return obj;
+    };
+
+    return obj;
+  };
   window.$ = $;
 })();
