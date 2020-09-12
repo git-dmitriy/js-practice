1af17dd1ef130b44e7d72bb51f8c186958f8aa64
Dmitriy <shalberkind@mail.ru>
2020-09-12 21:05:55 +0300

    Подключен модуль для управления DOM элементами

--- UI library/src/js/lib/lib.js
+++ UI library/src/js/lib/lib.js
@@ -2,6 +2,7 @@ import $ from "./core";
 import "./modules/display";
 import "./modules/classes";
 import "./modules/attributes";
+import "./modules/handlers";
 import "./modules/actions";
 
 export default $;
