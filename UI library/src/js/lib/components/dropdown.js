16b48b6b8ce5b70a35bb8985afa6716c69ab5365
Dmitriy <shalberkind@mail.ru>
2020-09-14 23:55:02 +0300

    Исправлена опечатка

--- UI library/src/js/lib/components/dropdown.js
+++ UI library/src/js/lib/components/dropdown.js
@@ -1,7 +1,7 @@
 import $ from "../core";
 
 $.prototype.dropdown = function () {
-  for (let i = 0; this.length; i++) {
+  for (let i = 0; i < this.length; i++) {
     const id = this[i].getAttribute("id");
     $(this[i]).click(() => {
       $(`[data-toggle-id="${id}"]`).fadeToggle(300);
