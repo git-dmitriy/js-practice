d471650fe78234f4f36c30391a89781020bd4ccc
Dmitriy <shalberkind@mail.ru>
2020-09-15 23:53:28 +0300

    Добавлен метод генерации модального окна из js

--- UI library/src/js/lib/components/modal.js
+++ UI library/src/js/lib/components/modal.js
@@ -28,6 +28,53 @@ $.prototype.modal = function () {
   });
 };
 
+$.prototype.createModal = function ({ text, btns } = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.setAttribute("id", this[i].getAttribute("data-target").slice(1));
+
+    // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+
+    console.log(btns);
+    const buttons = []; // todo Использовать деструктуризацию
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement("button");
+      btn.classList.add("btn", ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+
+      if (btns.settings[j][2]) {
+        btn.setAttribute("data-close", "true");
+      }
+      if (btns.settings[j][3] && typeof btns.settings[j][3] === "function") {
+        btn.addEventListener("click", btns.settings[j][3]);
+      }
+      buttons.push(btn);
+    }
+
+    modal.innerHTML = `
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <button class="close" data-close>
+            <span>&times;</span>
+          </button>
+          <div class="modal-header">
+            <div class="modal-title">${text.title}</div>
+          </div>
+          <div class="modal-body">${text.body}</div>
+          <div class="modal-footer">
+
+          </div>
+        </div>
+      </div>
+    `;
+    modal.querySelector(".modal-footer").append(...buttons);
+    document.body.appendChild(modal);
+    $(this[i]).modal();
+    $(this[i].getAttribute("data-target")).fadeIn(500);
+  }
+};
+
 $('[data-toggle="modal"]').modal();
 
 function _scrollWidth() {
