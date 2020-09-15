4f1809f8ae5f2bda508bb47ae94e771a88174005
Dmitriy <shalberkind@mail.ru>
2020-09-15 20:09:31 +0300

    Добавлена функция устраняющая смещение страницы при показе модальных окон

--- UI library/src/js/lib/components/modal.js
+++ UI library/src/js/lib/components/modal.js
@@ -8,7 +8,7 @@ $.prototype.modal = function () {
       e.preventDefault();
       $(target).fadeIn(500, "block");
       document.body.style.overflow = "hidden";
-      //    ! Устранить смещение контента при исчезновении полосы прокрутки
+      document.body.style.marginRight = `${_scrollWidth()}px`;
     });
   }
   const closeElements = document.querySelectorAll("[data-close]");
@@ -16,14 +16,32 @@ $.prototype.modal = function () {
     $(element).click(() => {
       $(".modal").fadeOut(500);
       document.body.style.overflow = "";
+      document.body.style.marginRight = "";
     });
   });
   $(".modal").click((e) => {
     if (e.target.classList.contains("modal")) {
       $(".modal").fadeOut(500);
       document.body.style.overflow = "";
+      document.body.style.marginRight = "";
     }
   });
 };
 
 $('[data-toggle="modal"]').modal();
+
+function _scrollWidth() {
+  let div = document.createElement("div");
+
+  div.style.width = "50px";
+  div.style.height = "50px";
+  div.style.overflowY = "scroll";
+  div.style.visibility = "hidden";
+
+  document.body.appendChild(div);
+
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
