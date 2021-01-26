96e549f346ca88cc5184e6ac7da09612b3faa8a5
Dmitriy <shalberkind@mail.ru>
2021-01-26 14:29:02 +0300

    Add lazy load for images

--- kartiny.art/src/js/main.js
+++ kartiny.art/src/js/main.js
@@ -12,30 +12,46 @@ import burger from "./modules/burger";
 import scrolling from "./modules/scrolling";
 import dragNdrop from "./modules/dragNdrop";
 
+import "lazysizes";
+
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   modals();
+
   sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn");
+
   sliders(".main-slider-item", "vertical");
+
   forms();
+
   mask("[name='phone']");
+
   checkTextInputs('[name="name"]');
+
   checkTextInputs('[name="message"]');
+
   showMoreStyles(".button-styles", "#styles .row");
+
   calc("#size", "#material", "#options", ".promocode", ".calc-price");
+
   filter();
+
   pictureSize(".sizes-block");
+
   accordion(".accordion-heading");
+
   burger(".burger-menu", ".burger");
+
   scrolling(".pageup");
+
   dragNdrop();
 });
 
 // + TODO Убрать эффект смещения виджета подарка при открытии и закрытии модальных окон.
 
 // TODO Добавить минимальный индекс положения курсора в форме
-// TODO Пофиксить баг с focus в chrome
+// +TODO Пофиксить баг с focus в chrome
 // TODO Исключить появление модальных окон при заполнении форм
 // todo Исключить возможность заполнения текстовых форм автозаполнением английскими буквами
 
