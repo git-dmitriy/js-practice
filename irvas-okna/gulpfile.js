cff0af9eca61233a4e70242120703d404aab352b
Dmitriy <shalberkind@mail.ru>
2021-01-29 11:00:55 +0300

    Create tasks for image minification

--- irvas-okna/gulpfile.js
+++ irvas-okna/gulpfile.js
@@ -11,6 +11,7 @@ const gcmq = require("gulp-group-css-media-queries");
 const cleanCSS = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
 const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
 
 sass.compiler = require("sass");
 
@@ -55,6 +56,31 @@ gulp.task("minify-html", () => {
     .pipe(browsersync.stream());
 });
 
+gulp.task("imagemin", () => {
+  return gulp
+    .src("./src/assets/img/**/*.*")
+    .pipe(plumber())
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest(dist + "assets/img"))
+    .on("end", browsersync.reload);
+});
+
+gulp.task("images", () => {
+  return gulp
+    .src("./src/assets/img/**/*.*")
+    .pipe(plumber())
+    .pipe(gulp.dest(dist + "/assets/img"));
+});
+
 gulp.task("build-js", () => {
   return gulp
     .src("./src/js/main.js")
@@ -97,7 +123,7 @@ gulp.task("build-js", () => {
 
 gulp.task("copy-assets", () => {
   return gulp
-    .src(["./src/assets/**/*.*", "!./src/assets/css/**/*.*"])
+    .src(["./src/assets/**/*.*", "!./src/assets/css/", "!./src/assets/img/"])
     .pipe(gulp.dest(dist + "/assets"))
     .on("end", browsersync.reload);
 });
@@ -112,12 +138,13 @@ gulp.task("watch", () => {
 
   gulp.watch("./src/index.html", gulp.parallel("copy-html"));
   gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets", "styles"));
+  gulp.watch("./src/assets/img/**/*.*", gulp.parallel("imagemin", "styles"));
   gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
 });
 
 gulp.task(
   "build",
-  gulp.parallel("copy-html", "copy-assets", "styles", "build-js")
+  gulp.parallel("copy-html", "copy-assets", "images", "styles", "build-js")
 );
 
 gulp.task("build-prod-js", () => {
@@ -163,6 +190,7 @@ gulp.task(
   gulp.series(
     "clean",
     "minify-html",
+    "imagemin",
     "copy-assets",
     "styles",
     "build-prod-js",
